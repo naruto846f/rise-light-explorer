3e1f187b2cc4f676dbe4e05ea34a0ad2081c982e
nandopatel <nandopatel2001@yahoo.co.uk>
2019-04-20 16:12:08 +0100

    migrate js

---
+++ script.js
@@ -0,0 +1,87 @@
+rise.nodeAddress = 'https://wallet.rise.vision';
+
+
+// if  docid of height < height, update & show alert (new block)
+function get_height(){
+    rise.blocks.getHeight(function(error, result) {
+        let height;
+        if (!error) {
+            alert_success('Retrieved current height.');
+            document.getElementById("height").innerHTML = result["height"];
+            height = result["height"];
+            document.getElementById('words').innerHTML = inWords(height);
+            return height;
+        } else {
+            alert_error('Cannot retrieve height at this time, check console for error');
+            console.log('error: ', error);
+        }
+
+    });
+}
+
+function alert_success(message){
+    $.notify({
+        // options
+        //icon: 'glyphicon glyphicon-warning-sign',
+        title: 'Success: ',
+        message: message
+    },{
+        // settings
+        type: 'success',
+        allow_dismiss: false,
+        newest_on_top: false,
+        delay: 5000,
+        placement: {
+            from: "bottom",
+            align: "right"
+        },
+        animate: {
+            enter: 'animated bounceIn',
+            exit: 'animated bounceOut'
+        }
+    });
+}
+
+function alert_error(message){
+    $.notify({
+        // options
+        //icon: 'glyphicon glyphicon-warning-sign',
+        title: 'Error: ',
+        message: message
+    },{
+        // settings
+        type: 'danger',
+        allow_dismiss: false,
+        newest_on_top: false,
+        delay: 5000,
+        placement: {
+            from: "bottom",
+            align: "right"
+        },
+        animate: {
+            enter: 'animated bounceIn',
+            exit: 'animated bounceOut'
+        }
+    });
+}
+
+
+$( document ).ready(function() {
+    get_height();
+    //setInterval(get_height,5000); change before commit
+})
+
+var a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
+function inWords (num) {
+    if ((num = num.toString()).length > 9) return 'overflow';
+    n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    if (!n) return; var str = '';
+    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
+    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
+    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+    str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'blocks ' : '';
+    return str;
+}

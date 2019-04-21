7afed028705358ffc3dcc2464541090c22f2e246
nandopatel <nandopatel2001@yahoo.co.uk>
2019-04-21 22:37:42 +0100

    sort js and add github button

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 rise.nodeAddress = 'https://wallet.rise.vision';
 
+$( document ).ready(function() {
+    alert_success('Connected');
+    setInterval(get_height,5000);
+});
+
 function get_height(){
     rise.blocks.getStatus(function(error, result) {
         let height = result["height"];
@@ -21,6 +26,58 @@ function get_height(){
             console.log('error: ', error);
         }
     });
+}
+
+function get_delegate(public_key){
+    rise.delegates.getByPublicKey(public_key).then(function({ delegate }) {
+        let username = delegate.username
+        $('#forger').text(username);
+        return username;
+    })
+        .catch(function(err) {
+            console.log('Error: ', err) // handle error
+        })
+}
+
+function block_info(id) {
+    rise.blocks.getBlock(id).then(function({ block }) {
+        console.log(block);
+    })
+        .catch(function(err) {
+            console.log('Error: ', err) // handle error
+        })
+}
+
+var a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
+function inWords (num) {
+    if ((num = num.toString()).length > 9) return 'overflow';
+    n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    if (!n) return; var str = '';
+    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
+    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
+    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+    str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'blocks ' : '';
+    return str;
+}
+
+function height_id() {
+    rise.blocks.getBlocks({limit: 1, orderBy:"height:desc"}).then(function ({blocks}) {
+        let id = blocks[0].id;
+        let ntxs = blocks[0].numberOfTransactions;
+        let txed = blocks[0].totalAmount/100000000;
+        let generatorPublicKey = blocks[0].generatorPublicKey;
+        get_delegate(generatorPublicKey);
+        console.log(block_info(id));
+        $('#id').text(id);
+        $('#ntxs').text(ntxs);
+        $('#txed').text(txed);
+        return id;
+    }).catch(function (err) {
+        console.log('Error: ', err) // handle error
+    })
 }
 
 function alert_success(message){
@@ -90,61 +147,4 @@ function alert_error(message){
             exit: 'animated bounceOut'
         }
     });
-}
-
-var a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
-var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
-
-function inWords (num) {
-    if ((num = num.toString()).length > 9) return 'overflow';
-    n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
-    if (!n) return; var str = '';
-    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
-    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
-    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
-    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
-    str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'blocks ' : '';
-    return str;
-}
-
-$( document ).ready(function() {
-    alert_success('Connected');
-    setInterval(get_height,5000);
-});
-
-function height_id() {
-    rise.blocks.getBlocks({limit: 1, orderBy:"height:desc"}).then(function ({blocks}) {
-        let id = blocks[0].id;
-        let ntxs = blocks[0].numberOfTransactions;
-        let txed = blocks[0].totalAmount/100000000;
-        let generatorPublicKey = blocks[0].generatorPublicKey;
-        get_delegate(generatorPublicKey);
-        console.log(block_info(id));
-        $('#id').text(id);
-        $('#ntxs').text(ntxs);
-        $('#txed').text(txed);
-        return id;
-    }).catch(function (err) {
-        console.log('Error: ', err) // handle error
-    })
-}
-
-function get_delegate(public_key){
-    rise.delegates.getByPublicKey(public_key).then(function({ delegate }) {
-            let username = delegate.username
-            $('#forger').text(username);
-            return username;
-        })
-        .catch(function(err) {
-            console.log('Error: ', err) // handle error
-        })
-}
-
-function block_info(id) {
-    rise.blocks.getBlock(id).then(function({ block }) {
-        console.log(block);
-    })
-    .catch(function(err) {
-        console.log('Error: ', err) // handle error
-    })
 }

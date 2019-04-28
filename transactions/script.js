6ecd3e7a48478256b88d32b13cb3862e86b21713
nandopatel <nandopatel2001@yahoo.co.uk>
2019-04-28 21:30:51 +0100

    fix #20

--- transactions/script.js
+++ transactions/script.js
@@ -11,14 +11,14 @@ function transactions() {
             console.log(transactions);
             let tid_link = 'https://explorer.rise.vision/tx/';
             //for (tx in range) {}
-            var i;
+            let i;
             for (i = 0; i < 5; i++) {
-                $('#sndr'+(i+1)).text((transactions[i].senderId).substring(0,5) + '...R');
-                $('#amnt'+(i+1)).text(Math.round(transactions[i].amount/100000000) + 'RISE');
-                $('#rcvr'+(i+1)).text((transactions[i].recipientId).substring(0,5)+'...R ');
-                $('#confs'+(i+1)).text(transactions[i].confirmations + ' blocks ago');
-                $('#tl'+(i+1)).attr("href", tid_link + transactions[i].id);
-            }
+                    $('#sndr'+(i+1)).text((transactions[i].senderId).substring(0,5) + '...R');
+                    $('#amnt'+(i+1)).text(Math.round(transactions[i].amount/100000000) + 'RISE');
+                    $('#rcvr'+(i+1)).text((transactions[i].recipientId).substring(0,5)+'...R ');
+                    $('#confs'+(i+1)).text(transactions[i].confirmations + ' blocks ago');
+                    $('#tl'+(i+1)).attr("href", tid_link + transactions[i].id);
+                }
         })
         .catch(function (err) {
             console.log('Error: ', err); // handle error
